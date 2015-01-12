10dae2e1e2efcd55d85e288e480f43d61282b7df
Joe Conradt <joe@conradtweb.com>
2015-01-12 14:38:08 +0000

    Change version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014122900;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2015011200;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2011112900;        // Requires this Moodle version.
 $plugin->component = 'local_sqlbuddy';
 $plugin->maturity  = MATURITY_STABLE;
